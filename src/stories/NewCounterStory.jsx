dd6e604352c5223190a0276113b7ce3919bda0ca
seol <seolnavy@naver.com>
2020-08-13 22:38:28 +0900

    storybook 환경 세팅 및 적용

---
+++ src/stories/NewCounterStory.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import NewCounter from '../03/NewCounter';
+
+storiesOf('NewCounter', module).addWithJSX('기본 설정', () => <NewCounter count={0} />);

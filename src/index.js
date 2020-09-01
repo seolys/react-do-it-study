f429a3553f6e1bb4943a31d464239455ab74101e
seol <seolnavy@naver.com>
2020-09-01 20:08:31 +0900

    상단메뉴 구성

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './doit-ui/app.css';
+import CoinApp from './08/CoinApp';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<CoinApp />, document.getElementById('root'));
+
+serviceWorker.unregister();

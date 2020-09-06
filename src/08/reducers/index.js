b9ca9ae4b81c335eca92728f00f043fc0cfaf295
seol <seolnavy@naver.com>
2020-09-06 14:46:59 +0900

    리듀서 모듈화

--- src/08/reducers/index.js
+++ src/08/reducers/index.js
@@ -1,9 +1,13 @@
-import transactions from './transactionsReducer';
+// import transactions from './transactionsReducer';
 import notification from './notificationReducers';
 import searchFilter from './searchFilterReducer';
+import createReducers from '../../11/api-redux-pack/createReducers';
+
+const apiReducers = createReducers('transactions');
 
 export default {
+  // transactions,
+  ...apiReducers,
   notification,
-  transactions,
   searchFilter,
 };
